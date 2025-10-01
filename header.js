ff98b44028c8a51c8dfdfc7007240b261d9871de
Cameron Taylor <camronun@gmail.com>
2025-10-02 08:15:49 +1000

    scrolling header implemented need to fix responsivity to different screen sizes

--- header.js
+++ header.js
@@ -1,7 +1,19 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a')
-navLinks.forEach(link => {
-    if(link.href.includes(`${activePage}`)){
-        link.classList.add('active')
-    }
-})
+let sections = document.querySelectorAll('.section');
+let navLinks = document.querySelectorAll('.navoption')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+       let top = window.scrollY;
+       let offset = sec.offsetTop
+       let height = sec.offsetHeight 
+       let id = sec.getAttribute('id');
+       console.log(top, offset, height, id)
+
+       if(top >= offset && top < offset + height ) {
+        navLinks.forEach(links => {
+            links.classList.remove('active')
+            document.querySelector('.navoption[href*=' + id +']').classList.add('active');
+        })
+       }
+    });
+}
